539a2e2c9cad1f53abeec0c801a4c800f8e12aa6
DiamondCrack <zk05161219@gmail.com>
2017-11-03 22:41:04 +0800

    My Emacs configuration files backed up

---
+++ elpa/window-jump-20170809.1508/window-jump-autoloads.el
@@ -0,0 +1,41 @@
+;;; window-jump-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "window-jump" "window-jump.el" (22986 13859
+;;;;;;  0 0))
+;;; Generated autoloads from window-jump.el
+
+(autoload 'window-jump-left "window-jump" "\
+Move to the window to the left of the current window.
+
+\(fn)" t nil)
+
+(autoload 'window-jump-right "window-jump" "\
+Move to the window to the right of the current window.
+
+\(fn)" t nil)
+
+(autoload 'window-jump-down "window-jump" "\
+Move to the window below the current window.
+
+\(fn)" t nil)
+
+(autoload 'window-jump-up "window-jump" "\
+Move to the window above the current window.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("window-jump-pkg.el") (22986 13847 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; window-jump-autoloads.el ends here
---
+++ elpa/window-jump-20170809.1508/window-jump-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "window-jump" "20170809.1508" "Move left/right/up/down through your windows." 'nil :commit "6bdb51e9a346907d60a9625f6180bddd06be6674" :url "https://github.com/chumpage/chumpy-windows" :keywords '("frames" "convenience"))
